1d9619f04f7abe4ebee5b946c1cabd132e2e3f7d
Juanitox98 <juanestebanacostarodriguez@gmail.com>
2025-09-15 16:43:39 -0500

    septimo commit

---
+++ Assets/scripts/MusicManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager instance;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = true;  
+        audioSource.Play();       
+    }
+}
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,36 +7,77 @@ public class PlayerController : MonoBehaviour
     private Vector3 posicion;
     public float speed;
     private Rigidbody rb;
-    // Start is called before the first frame update
+    public Transform particulas;
+    private ParticleSystem systemaParticulas;
+
+    private AudioSource[] audioSources; 
+    private AudioSource choqueAudio;      
+    private AudioSource recolectarAudio;
+    private AudioSource rampaAudio; 
+
     void Start()
     {
-        rb = GetComponent<Rigidbody> ();
+        rb = GetComponent<Rigidbody>();
+        audioSources = GetComponents<AudioSource>();
+        systemaParticulas = particulas.GetComponent<ParticleSystem>();
+        systemaParticulas.Stop();
+
+        if (audioSources.Length >= 2)
+        {
+            choqueAudio = audioSources[0];       
+            recolectarAudio = audioSources[1];
+            rampaAudio = audioSources[2];   
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
-    }
-    void FixedUpdate (){
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movimiento = new Vector3 (moveHorizontal,0.0f,moveVertical);
-
-        rb.AddForce (movimiento*speed);
+        Vector3 movimiento = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movimiento * speed);
     }
+
     void OnTriggerEnter(Collider other)
     {
+        // Caso: recolectable
         if (other.gameObject.CompareTag("Recolectable"))
         {
+            posicion = other.gameObject.transform.position;
+            particulas.position = posicion;
+            systemaParticulas.Play();
             other.gameObject.SetActive(false);
-           
-        } else {
-          
+
+            if (recolectarAudio != null)
+                recolectarAudio.Play();
         }
-        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Caso: pared
+        if (collision.gameObject.CompareTag("Pared"))
+        {
+            if (choqueAudio != null)
+                choqueAudio.Play();
+        }
+
+       
+        if (collision.gameObject.CompareTag("Rampa"))
+        {
             
-        
+            StartCoroutine(DesactivarRampa(collision.gameObject));
+        }
+    }
+
+    IEnumerator DesactivarRampa(GameObject rampa)
+    {
+        yield return new WaitForSeconds(1f);
+        if (rampaAudio != null)
+            rampaAudio.Play();
+            
+        rampa.SetActive(false);
     }
 }
+
